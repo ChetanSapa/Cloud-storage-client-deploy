cba1e69e701ae35aee94d44f7e350ac10c8f6f0d
Damian <errollgarner@yandex.ru>
2023-04-18 14:32:31 +0200

    fix headers CORS

--- src/actions/user.js
+++ src/actions/user.js
@@ -37,7 +37,7 @@ export const auth = (email, password) => {
     return async dispatch => {
         try {
             const response = await axios.get(`${API_URL}api/auth/auth/`,
-                {headers: {Authorization: `Bearer: ${localStorage.getItem('token'),CORS: 'Access-Control-Allow-Origin'}`}}
+                {headers: {Authorization: `Bearer: ${localStorage.getItem('token')}`,CORS: 'Access-Control-Allow-Origin'}}}
             )
             dispatch(setUser(response.data.user))
             localStorage.setItem('token', response.data.token)
